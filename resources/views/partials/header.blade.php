ecbdbc1a891262a81b6f616bcc726c171d8c01db
Lucianor01 <lucianorosario02@gmail.com>
2023-06-08 17:11:48 +0200

    img pages layout

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,42 @@
+<header>
+    <nav>
+        <div>
+            <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="Logo">
+        </div>
+
+        <div>
+            <ul>
+                <li>
+                    <a href="#"><strong>CHARACTERS</strong></a>
+                </li>
+                <li>
+                    <a href="#"><strong>COMICS</strong></a>
+                </li>
+                <li>
+                    <a href="#"><strong>MOVIES</strong></a>
+                </li>
+                <li>
+                    <a href="#"><strong>TV</strong></a>
+                </li>
+                <li>
+                    <a href="#"><strong>GAMES</strong></a>
+                </li>
+                <li>
+                    <a href="#"><strong>COLLECTIBLES</strong></a>
+                </li>
+                <li>
+                    <a href="#"><strong>VIDEOS</strong></a>
+                </li>
+                <li>
+                    <a href="#"><strong>FANS</strong></a>
+                </li>
+                <li>
+                    <a href="#"><strong>NEWS</strong></a>
+                </li>
+                <li>
+                    <a href="#"><strong>SHOP</strong></a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
